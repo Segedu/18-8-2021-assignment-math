44407a21e43633876674afd124a0e1f8639709e2
segedu <seggedu20@gmail.com>
2021-08-19 19:38:17 +0300

    math assignment jacob

---
+++ Math.js
@@ -0,0 +1,50 @@
+// //1
+// console.log(Math.floor(Math.random() * 100));
+
+// //2
+// console.log(Math.floor(Math.random() * 10000));
+
+// //3
+// console.log(Math.floor(Math.random() * 55));
+
+// //4
+// var userInput = prompt("enter number");
+// function printRandomByUserInput(someNumber) {
+//   console.log(Math.floor(Math.random() * someNumber));
+// }
+// printRandomByUserInput(userInput);
+
+// //5
+// var userInput = prompt("enter number from 0 to 9");
+// var random = Math.floor(Math.random() * 10);
+
+// function isUserInputEqualRandom(randomNum,someNumber) {
+// if (randomNum == someNumber) {
+// return true;
+// }
+// console.log(isUserInputEqualRandom(random, userInput));
+
+////6
+// var userInput = prompt("enter number from 0 to 99");
+// var randomNumber = Math.floor(Math.random() * 100);
+
+// function isUserInputEqualRandom(random, userNumber) {
+//   if (random == userNumber) {
+//     return true;
+//   }
+//   return false;
+// }
+// console.log(isUserInputEqualRandom(randomNumber, userInput));
+
+////7
+// var userName = prompt("enter name");
+// var userInputNum = prompt("enter number from 0 to 9");
+// var random = Math.floor(Math.random() * 3);
+
+// function nameWithWinOrLoose(firstName, userNumber, randomNum) {
+//   if (randomNum == userNumber) {
+//     document.write(`${firstName} "you win"`);
+//   }
+//   return false;
+// }
+// console.log(nameWithWinOrLoose(userName, userInputNum, random));
